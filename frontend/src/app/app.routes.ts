202788dd692127b770d7421f0cf0996f0eef9e60
irina2405 <155219238+irina2405@users.noreply.github.com>
2026-02-17 15:30:43 +0300

    base site vitrine

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard }
+];
---
+++ frontend/src/app/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { Navbar } from '../../components/navbar/navbar';
+import { Hero } from '../../components/hero/hero';
+import { Services } from '../../components/services/services';
+import { Contact } from '../../components/contact/contact';
+import { Footer } from '../../components/footer/footer';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    Navbar,
+    Hero,
+    Services,
+    Contact,
+    Footer
+  ],
+  templateUrl: './home.html'
+})
+export class Home {}
