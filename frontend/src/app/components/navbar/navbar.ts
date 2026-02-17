4aeffdaa2329f80023360c65bf6df3c5cf807343
irina2405 <155219238+irina2405@users.noreply.github.com>
2026-02-17 17:19:33 +0300

    init site vitrine 1.2

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const heroHeight = window.innerHeight * 0.8;
+    this.isScrolled = window.scrollY > heroHeight;
+  }
 }
