1cfa97b1ffa36e301b7e513657869b7baa0b034e
irina2405 <155219238+irina2405@users.noreply.github.com>
2026-02-18 16:04:16 +0300

    passage via url login

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -5,7 +5,6 @@ import { Hero } from '../../components/hero/hero';
 import { Services } from '../../components/services/services';
 import { Contact } from '../../components/contact/contact';
 import { Footer } from '../../components/footer/footer';
-
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -14,7 +13,7 @@ import { Footer } from '../../components/footer/footer';
     Hero,
     Services,
     Contact,
-    Footer
+    Footer,
   ],
   templateUrl: './home.html'
 })
