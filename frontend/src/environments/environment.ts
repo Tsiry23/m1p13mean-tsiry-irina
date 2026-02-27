44ccb63375027dc406accced7dbbff1afdda93a6
Tsiry23 <144609925+Tsiry23@users.noreply.github.com>
2026-02-27 12:04:01 +0300

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // apiBaseUrl: 'http://localhost:3000'
   // apiBaseUrl: 'http://backend:3000'
-  apiBaseUrl: '/api'
+  apiBaseUrl: 'https://m1p13mean-tsiry-irina-1.onrender.com'
 };
